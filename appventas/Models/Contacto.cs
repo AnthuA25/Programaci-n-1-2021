6ad3e68382a0964f38c7876b72401aea97a2f360
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-05-22 22:01:03 -0500

    app de ventas

---
+++ appventas/Models/Contacto.cs
@@ -0,0 +1,11 @@
+namespace appventas.Models
+{
+    public class Contacto
+    {
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+        public string Phone {get; set;}
+
+    }
+}
